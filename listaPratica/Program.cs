a560b9faa057bc638a8faa7a238187756d630ed1
Rocha Lucas <l.rocha@reply.com>
2020-05-29 10:50:53 -0300

    primeiro upload

---
+++ listaPratica/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace listaPratica
+{
+    class Program
+    {
+        static String caminhoArquivo = @"C:\var\grafo.txt";
+        public static void lerArquivo()
+        {
+            using (StreamReader sr = File.OpenText(caminhoArquivo))
+            {
+                String[] documento = File.ReadAllLines(caminhoArquivo);
+
+                foreach (string linha in documento)
+                {
+                    string[] dados = linha.Split(' ');
+                    Console.WriteLine(dados[0] +" " + dados[1]);
+                }
+
+                    
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            lerArquivo();
+          
+            Console.ReadKey();
+        }
+    }
+}
