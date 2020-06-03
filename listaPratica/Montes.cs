a20f93a931010ba82d3815f7e21d0525258593be
Rocha Lucas <l.rocha@reply.com>
2020-06-02 22:05:15 -0300

    Custo

--- listaPratica/Montes.cs
+++ listaPratica/Montes.cs
@@ -10,12 +10,14 @@ namespace listaPratica
     {
         public int monteInicial;
         public int monteFinal;
+        public int custo;
         public List<PosicoesPesos> ListaPosicoesPesos;
 
         public Montes()
         {
             monteInicial = 0;
             monteFinal = 0;
+            custo = 0;
             ListaPosicoesPesos = new List<PosicoesPesos>();
         }
     }
